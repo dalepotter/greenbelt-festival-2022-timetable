3842b0ceeb4b91a3db2b3409740cc8b1e7c5ecca
dalepotter <dalepotter@gmail.com>
2022-08-26 08:19:30 +0100

    Crawl Greenbelt website for artist and event data

---
+++ greenbelt_festival_2022_timetable/spiders/timetable.py
@@ -0,0 +1,64 @@
+import re
+import scrapy
+from datetime import datetime, timedelta
+
+
+class LineupSpider(scrapy.Spider):
+    """Crawls the Greenbelt Festival 2022 lineup to extract artist appearance data."""
+    name = "greenbelt_festival_2022_lineup"
+    start_urls = ["https://www.greenbelt.org.uk/2022-lineup/"]
+
+    def parse(self, response):
+        self.logger.info("Starting Greenbelt 2022 timetable spider")
+        all_artists = response.css("ul.listing").css("li")
+        for artist in all_artists:
+            artist_data = {
+                "artist_name": artist.xpath("string(a)").get(),
+                "href": artist.xpath("a/@href").get(),
+                "tags": artist.xpath("@class").get(),
+                "tags_set": set(artist.xpath("@class").get().strip().split(" "))
+            }
+            yield response.follow(
+                artist_data["href"],
+                callback=self.parse_artist,
+                cb_kwargs={'artist_data': artist_data}
+            )
+
+    def parse_artist(self, response, artist_data):
+        self.logger.info(f"Extracting data for artist: {artist_data['artist_name']}")
+        all_events = response.xpath("//aside[contains(@class, 'shows')]")
+        for event in all_events:
+            # Extract date and time
+            event_day_time_elem = event.xpath("h3/br[1]/following-sibling::text()[1]")
+            if "Friday" in event_day_time_elem.get():
+                base_date = datetime(2022, 8, 26)
+            elif "Saturday" in event_day_time_elem.get():
+                base_date = datetime(2022, 8, 27)
+            elif "Sunday" in event_day_time_elem.get():
+                base_date = datetime(2022, 8, 28)
+            elif "Monday" in event_day_time_elem.get():
+                base_date = datetime(2022, 8, 29)
+            event_day_time_elem.re("\d\d:\d\d")[0]
+            event_time = re.search(r"(?P<hours>\d\d):(?P<mins>\d\d)", event_day_time_elem.get())
+            event_datetime = base_date + timedelta(
+                hours=int(event_time.group("hours")),
+                minutes=int(event_time.group("mins"))
+            )
+
+            # yield {
+            #     "artist_name": artist_data["artist_name"],
+            #     "event_location": event.xpath("h3/br[1]/preceding-sibling::text()[1]").get(),
+            #     "event_title": event.xpath("string(h4)").get(),
+            #     "event_day": event_datetime.strftime("%A"),
+            #     "event_start_time": event_datetime.strftime("%H:%M"),
+            #     "event_summary": event.xpath("string(p)").get(),
+            #     "source_url": response.url
+            # }
+
+            yield {
+                "act": event.xpath("string(h4)").get(),
+                "start": event_datetime,
+                "stage": event.xpath("h3/br[1]/preceding-sibling::text()[1]").get()
+            }
+
+            #"start":"2016-06-24 18:15","end":"2016-06-24 19:15","stage":"Pyramid","act":"Jess Glynne"
